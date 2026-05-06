31a28ede6bad581ddf745772b8e962bc9ac673c2
Ayaan Adrito <74221075+Ayaanator@users.noreply.github.com>
2026-05-05 20:34:55 -0400

    Add: eating-bananas - submission-7

---
+++ Data Structures & Algorithms/eating-bananas/submission-7.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int findLargest(vector<int>& arr) {
+        int mx = -1;
+
+        for(auto x : arr) {
+            mx = max(x, mx);
+        }
+
+        return mx;
+    }
+
+    int findSmallest(vector<int>& arr, int biggest) {
+        int mn = biggest;
+
+        for(auto x : arr) {
+            mn = min(x, mn);
+        }
+
+        return mn;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int left = 1;
+        int right = findLargest(piles);
+        int og_biggest = right;
+        int mid = 0;
+        vector<int> mids;
+        
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            long long hours = 0;
+
+            for(int i = 0; i < piles.size(); i++) {
+                hours += (long long)(piles[i] + mid - 1) / mid;
+            }
+
+            if (hours > h) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+
+            if(hours <= h) {
+                mids.push_back(mid);
+            }
+        }
+
+        return findSmallest(mids, og_biggest);
+    }
+};
